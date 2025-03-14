83221fe119cb07fc0e8525f8a3c14be1b333f444
olek-dot-com <aleksander.maciejewski@student.put.poznan.pl>
2025-03-14 12:09:42 +0100

    Trying to create something on my own

---
+++ fakebankproject/src/main/java/org/fakebankproject/fakebankproject/FakebankprojectApplication.java
@@ -0,0 +1,13 @@
+package org.fakebankproject.fakebankproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakebankprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakebankprojectApplication.class, args);
+	}
+
+}
