0993b9d60f16c70db5357616c6159feffa903a59
olek-dot-com <aleksander.maciejewski@student.put.poznan.pl>
2025-03-15 14:23:52 +0100

    first settings. Just experimenting with IDE, trying to connect PostgreSQL. Courious how DB credentials are stored in projects developed by many people. Wondering how to host it safety in the cloud. First Classes created.

---
+++ fakebankproject/src/main/java/Service/Account.java
@@ -0,0 +1,7 @@
+package Service;
+
+public class Account {
+    public Account() {
+
+    }
+}
--- fakebankproject/src/main/java/org/fakebankproject/fakebankproject/FakebankprojectApplication.java
+++ fakebankproject/src/main/java/org/fakebankproject/fakebankproject/FakebankprojectApplication.java
@@ -9,5 +9,4 @@ public class FakebankprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FakebankprojectApplication.class, args);
 	}
-
 }
